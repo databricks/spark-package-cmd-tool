0e9069f22b1330e1b261f7171c61c10b54dd5355
Burak Yavuz <brkyvz@gmail.com>
2015-09-30 12:02:26 -0700

    fix py3-9

--- python/tests.py
+++ python/tests.py
@@ -242,7 +242,7 @@ def check_jar(test, jar, files):
     Check the contents of the pom. Make sure the groupId, artifactId, and version are properly set.
     :param files: List of entries expected in the jar
     """
-    jar_file = zipfile.PyZipFile(StringIO(jar.read().encode('utf-8')), 'rb')
+    jar_file = zipfile.PyZipFile(StringIO(jar.read()), 'rb')
     entries = jar_file.namelist()
     for expected in files:
         test.assertTrue(expected in entries)
